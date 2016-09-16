ddf9411b798d3ea9a75838bf3f49092f513361ba
Daniel-PC\Daniel <danielhuamani15@gmail.com>
2016-09-16 07:06:14 -0500

    agregando canbios nosotros index

--- my_apps/web/templatetags/app_tags.py
+++ my_apps/web/templatetags/app_tags.py
@@ -9,3 +9,17 @@ def counter_cero(num):
     num_cero = format(num, '02d')
 
     return num_cero
+
+
+@register.filter(name='tags_telefono_header')
+def tags_telefono_header(list_telefono):
+    telefonos = list_telefono.split("|")
+
+    return telefonos[0]
+
+
+@register.filter(name='tags_telefono_footer')
+def tags_telefono_footer(list_telefono):
+    telefonos = list_telefono.split("|")
+
+    return telefonos
